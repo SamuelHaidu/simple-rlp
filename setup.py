4d234fe126f898f16e1a7b9fa6f255fa319cf624
Samuel Haidu <shaidu@gifconsulting.com>
2020-06-11 22:13:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 
+with open('README.md', 'r') as readme_file:
+    README = readme_file.read()
+
 setup(
     name='simple-rlp',
     version='0.1.0',
     packages=['rlp'],
-    url='',
-    license='MIT',
+    url='https://github.com/SamuelHaidu/simple-rlp',
+    license='MIT Custom',
     author='Samuel Haidu',
     author_email='samuelhaidu3@gmail.com',
-    description='RLP (Recursive Length Prefix) - Encode and decode data structures'
+    description='RLP (Recursive Length Prefix) - Encode and decode data structures',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6'
 )
