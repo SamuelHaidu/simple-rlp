5c6469f6fa303d7d8720926be26f9557175507a3
Samuel Haidu <shaidu@gifconsulting.com>
2020-06-11 23:32:13 -0300

    Update setup.py version 0.1.1 - bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme_file:
 
 setup(
     name='simple-rlp',
-    version='0.1.0',
+    version='0.1.1',
     packages=['rlp'],
     url='https://github.com/SamuelHaidu/simple-rlp',
     license='MIT Custom',
